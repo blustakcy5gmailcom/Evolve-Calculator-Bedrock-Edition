72ec341d5ed9e1045c556b4b14bfceaf7de8ffac
blustakcy5gmailcom <146412924+blustakcy5gmailcom@users.noreply.github.com>
2024-09-07 16:49:04 +1000

    Update unix.sh

--- run/unix.sh
+++ run/unix.sh
@@ -2,7 +2,7 @@
 
 content=$(curl -s https://raw.githubusercontent.com/blustakcy5gmailcom/Evolve-Calculator-Bedrock-Edition/main/main.ts)
 
-temp_file=$(mktemp -t temp_script_evolvecalculator.XXXXXX.ts)
+temp_file=$(mktemp -t temp_script_evolvecalculator.ts)
 
 echo "$content" > "$temp_file"
 
