f1dd81e77dda76e91ed7c029811237d37c541078
blustakcy5gmailcom <146412924+blustakcy5gmailcom@users.noreply.github.com>
2024-09-07 16:53:57 +1000

    Update unix.sh

--- run/unix.sh
+++ run/unix.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-content=$(curl -s https://raw.githubusercontent.com/blustakcy5gmailcom/Evolve-Calculator-Bedrock-Edition/main/main.ts)
+curl -s https://raw.githubusercontent.com/blustakcy5gmailcom/Evolve-Calculator-Bedrock-Edition/main/main.ts > temp_evolvecalculator.ts
 
-temp_file="./temp_evolvecalculator.ts"
-
-echo "$content" > "$temp_file"
-
-npx ts-node --esm "$temp_file"
-
-rm "$temp_file"
+npx ts-node ./temp_evolvecalculator.ts
+rm ./temp_evolvecalculator.ts
