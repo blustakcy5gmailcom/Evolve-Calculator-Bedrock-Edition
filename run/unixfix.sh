ca74e4331771c8c69a7bde867c825bc3c6b17bb3
blustakcy5gmailcom <146412924+blustakcy5gmailcom@users.noreply.github.com>
2024-09-07 16:58:12 +1000

    Update unixfix.sh

--- run/unixfix.sh
+++ run/unixfix.sh
@@ -3,4 +3,3 @@
 curl -s https://raw.githubusercontent.com/blustakcy5gmailcom/Evolve-Calculator-Bedrock-Edition/main/main.ts > temp_evolvecalculator.ts
 
 npx ts-node ./temp_evolvecalculator.ts
-rm ./temp_evolvecalculator.ts
